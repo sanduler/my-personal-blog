2ca5b7c86c647a5fc5fc400d7e4aa54140459ffc
Ruben Sanduleac <ruvim.sanduleac@gmail.com>
2022-03-13 21:56:20 -0700

    commented the blog correctly

--- main.py
+++ main.py
@@ -11,26 +11,39 @@ app = Flask(__name__)
 post_objects = []
 # get the endpoint url
 url_blog = 'https://api.npoint.io/c790b4d5cab58020d391'
+# get the response from the url
 response = requests.get(url_blog)
+# get all the posts as a json dictionary
 all_posts = response.json()
-# print(all_posts)
-
+# use a for loop to loop through all the posts and send them to the Post class while creating them as objects
+# for each post ==> each post == object
 for post in all_posts:
+    # create an object for each post
     post_obj = Post(post['id'], post['title'], post['subtitle'], post['body'])
+    # append the post object to the list
     post_objects.append(post_obj)
 
 
+# get the home page
 @app.route('/')
 def home():
+    # return the all the post objects back to the html page
     return render_template("index.html", all_posts=post_objects)
 
+
+# rederect to page based on the post id --> post/{id}
 @app.route("/post/<int:index>")
 def show_post(index):
+    # set the requested_post to none
     requested_post = None
+    # loop through the list of post objects
     for blog_post in post_objects:
+        # when the id matches the selected index by the user the requested_post is set to the correct blog post
         if blog_post.id == index:
             requested_post = blog_post
+    # requested blog post is sent back to the post html
     return render_template("post.html", post=requested_post)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
